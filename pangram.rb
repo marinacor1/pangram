e76e89be33e5ac3f54c1099b30a711a6d479f04d
marinacor1 <marinaspam1234@gmail.com>
2016-08-12 11:19:21 -0600

    Basic structure implemented

--- pangram.rb
+++ pangram.rb
@@ -7,17 +7,28 @@ class Pangram
       letter_count = Hash.new()
       letters = str.split('')
       counter = 0
-      self.create_count(letters, letter_count, counter)
+      letter_count = self.create_count(letters, letter_count, counter)
+      self.all_letters(letter_count)
     end
   end
 
   def self.create_count(letters, letter_count, counter)
     letters.each do |letter|
-      if letter_count.keys == letter
+      if letter_count.keys.include?(letter)
         letter_count[letter] = counter + 1
       else
         letter_count[letter] = counter
       end
+      letter_count
+    end
+    letter_count
+  end
+
+  def self.all_letters(letter_count)
+    if letter_count.keys.length == 26
+      true
+    else
+      false
     end
   end
 
